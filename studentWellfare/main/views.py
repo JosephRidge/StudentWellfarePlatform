052c84dc3c5a6d0193230c97e092a93559795f29
JosephRidge <josephridge18@gmail.com>
2026-05-25 14:03:59 +0300

    update

--- studentWellfare/main/views.py
+++ studentWellfare/main/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login')
 def home(request):
     context ={} 
     print(request.user)
     return render(request, 'home.html', context)
+    
+@login_required(login_url='login')
+def userProfile(request):
+    context = {}  
+    return render(request, 'main/userprofile.html', context)
