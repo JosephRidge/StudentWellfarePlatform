7d62cb5ab75b85c0f389fc9ce65024f1dd9a7bb5
JosephRidge <josephridge18@gmail.com>
2026-05-29 14:23:57 +0300

    update

--- studentWellfare/main/urls.py
+++ studentWellfare/main/urls.py
@@ -3,5 +3,25 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name = 'home'),
-    path('user-profile/',views.userProfile, name='user-profile')
+    path('user-profile/',views.userProfile, name='user-profile'),
+
+# Reports CRUD
+    path('create-report/',views.createReport, name='create-report'),
+
+
+# APPOINTMENTS CRUD
+    path('book-appointment/',views.bookAppointment, name='book-appointment'),
+
+
+# FEEBACK CRUD
+    path('create-feedback/',views.createFeedback, name='create-feedback'),
+
+
+# RESEOUCE CRUD    
+    path('create-resource/',views.createResource, name='create-resource'),
+
+
+# NOTIFICAITON CRUD 
+    path('create-notification/',views.createNotification, name='create-notification'),
+
 ]
--- studentWellfare/main/views.py
+++ studentWellfare/main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import ReportForm, AppointmentForm, FeedbackForm, ResourceForm, NotificationForm
+from django.contrib import messages # aids in notification/ pop ups 
 
 # Create your views here.
 @login_required(login_url='login')
@@ -12,3 +14,51 @@ def home(request):
 def userProfile(request):
     context = {}  
     return render(request, 'main/userprofile.html', context)
+
+@login_required(login_url='login')
+def createReport(request):
+    form = ReportForm() # instance of the form 
+    context = {"form": form, "type": "Report"}
+    return render(request, 'main/forms.html', context)
+
+
+@login_required(login_url='login')
+def bookAppointment(request):
+    form = AppointmentForm() # instance of the form 
+    context = {"form": form, "type": "Book an AppointmentForm"}
+    return render(request, 'main/forms.html', context)
+
+
+@login_required(login_url='login')
+def createFeedback(request):
+    form = ResourceForm() # instance of the form 
+    context = {"form": form, "type": "How was your appointment? "}
+    return render(request, 'main/forms.html', context)
+
+
+@login_required(login_url='login')
+def createResource(request):
+    if request.method == "POST": 
+        form = ResourceForm(request.POST, request.FILES) # captures both for, input and files
+
+        if form.is_valid(): # check whether form inputs are well done
+            user = form.save(commit = False) # pause submission
+            user.save() # create the new user
+            messages.info(request, "Success!")
+            return redirect('login')
+        else:
+            # form = UserForm() # create an instance of the userform
+            messages.info(request, f"Failed! {form.errors}")
+    else:
+        messages.error(request, "Oops something went wrong")
+        form = ResourceForm() 
+    context = {"form": form, "type": "Add a new resource"}
+    return render(request, 'main/forms.html', context)
+
+
+
+@login_required(login_url='login')
+def createNotification(request):
+    form = NotificationForm() # instance of the form 
+    context = {"form": form, "type": "Notify users of something new!"}
+    return render(request, 'main/forms.html', context)
